8fcb1df04ce12447ec21f29d3868e943f7195a3d
pwells0318 <50330945+pwells0318@users.noreply.github.com>
2019-05-06 10:30:24 -0400

    Add files via upload

---
+++ ShinyAppv4.R
@@ -0,0 +1,103 @@
+library(shiny)
+library(shinyWidgets)
+
+# Define UI for app ----
+ui <- fluidPage(
+
+  setBackgroundColor(
+    color = c("#F7FBFF", "#e6e6fa"),
+    gradient = "linear",
+    direction = "bottom"
+  ),
+  
+  # App title ----
+  titlePanel("Technical Pricing Dashboard"),
+  
+  # Sidebar layout with input and output definitions ----
+  sidebarLayout(
+    
+    # Sidebar panel for inputs ----
+    sidebarPanel(
+      textInput("text1", label = h4("NAICS Code"), value = "Enter NAICS Code"),
+      textInput("text2", label = h4("Revenue"), value = "Enter Company Revenue"),
+      selectInput("select1", label = h4("Territory"), 
+                  choices = list("Select Territory" = 1,"Territory 1" = 2, "Territory 2" = 3, "Territory 3" = 4, "Territory 4" = 5), 
+                  selected = 1),
+      radioButtons("radio1", label = h3("Prior Insurance?"),
+                   choices = list("Yes" = 1, "No" = 2), 
+                   selected = 1),
+      
+      hr(),
+      fluidRow(column(3, verbatimTextOutput("value")))
+      
+    ),
+    
+    # Main panel for displaying outputs ----
+    mainPanel(
+      
+      # Output: Box Plot ----
+      h4("Summary of Key Risk Characteristics"),
+      plotOutput(outputId = "boxplot")
+      
+    )
+  )
+)
+
+server <- function(input, output) {
+  
+  output$boxplot <- renderPlot({
+
+    a=rnorm(50000 , mean=0 , sd=30)
+    b=rnorm(50000 , mean=40 , sd=100)
+    c=rnorm(50000 , mean=100 , sd=50)
+    d=rnorm(50000 , mean=20 , sd=20)
+    e=rnorm(50000 , mean=30 , sd=60)
+    f=rnorm(50000 , mean=100 , sd=30)
+    g=rnorm(50000 , mean=20 , sd=25)
+    h=rnorm(50000 , mean=40 , sd=100)
+    
+    apoint = 25
+    bpoint = 75
+    cpoint = 60
+    dpoint = 10
+    epoint = 35
+    fpoint = 175
+    gpoint = 20
+    hpoint = 20
+    
+    #margins around box plot
+    par(mar=c(5,4,2,2))
+    
+    #Make the boxplot
+    boxplot(a,b,c,d,e,f,g,h, 
+            xlab="Risk Characteristic" , 
+            ylab="Factor" ,
+            ylim=c(-300,300),
+            xlim=c(1,8),
+            col=cm.colors(8),
+            names = c("Char 1", "Char 2", "Char 3", "Char 4", "Char 5", "Char 6", "Char 7", "Char 8"),
+            outline = FALSE
+    )
+     
+    #add scatter plot of factors of the input risk to overlay the box plots
+    par(new=TRUE)
+    plot(1:8, 
+         c(apoint,bpoint,cpoint,dpoint,epoint,fpoint,gpoint,hpoint), 
+         axes=FALSE, 
+         ylab="", 
+         xlab="", 
+         ylim=c(-300,300), 
+         xlim = c(1,8), 
+         lwd = 5,
+         col="blue"
+         )
+    abline( h=seq(-300,300,50), 
+            v=seq(1,8) , 
+            col="grey80",
+            lty="dotted",
+            lwd = 0.4)
+  })
+  
+}
+
+shinyApp(ui = ui, server = server)
